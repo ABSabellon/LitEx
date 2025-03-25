c6a310e18ac6876fd9735d072d32dd59ee493933
aileen <aileen@seditio.ie>
2025-03-26 03:36:03 +0800

    updated to native wind

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,6 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-// Add support for CSS files
-config.resolver.sourceExts.push('css');
-// Add support for CJS files
-config.resolver.sourceExts.push('cjs');
-
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' })
